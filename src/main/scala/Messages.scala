e7e836a06491e848dfbc86db3bfe7e137df7d77f
Alexandre Berthaud <alexandre@berthaud.me>
2013-08-17 21:58:29 +0200

    Add: Debug messages
    Control each motor independently

--- src/main/scala/Messages.scala
+++ src/main/scala/Messages.scala
@@ -8,6 +8,9 @@ object Messages {
   def setDirection(x: Int)       = "DI" + _formatNumber(x)
   def setFrontLights(x: Boolean) = "LF" + (if(x) 1 else 0)
   def setSideLights(x: Boolean)  = "LS" + (if(x) 1 else 0)
+  def setDebugMA(x: Int)         = "MA" + _formatNumber(x)
+  def setDebugMB(x: Int)         = "MB" + _formatNumber(x)
+  def setDebugMC(x: Int)         = "MC" + _formatNumber(x)
 
   def _formatNumber(x: Int): String = (if(x >= 0) "+" else "-") + math.abs(x)
 
@@ -18,12 +21,18 @@ object Messages {
     speed: (Int) => String,
     direction: (Int) => String,
     frontLights: (Boolean) => String,
-    sideLights: (Boolean) => String
+    sideLights: (Boolean) => String,
+    debugMA: (Int) => String,
+    debugMB: (Int) => String,
+    debugMC: (Int) => String
   ) = (s: String) => {
     val speedPattern = "SP([-\\+])([0-9]+)".r
     val directionPattern = "DI([-\\+])([0-9]+)".r
     val frontLightsPattern = "LF([01])".r
     val sideLightsPattern = "LS([01])".r
+    val debugMAPattern = "MA([-\\+])([0-9]+)".r
+    val debugMBPattern = "MB([-\\+])([0-9]+)".r
+    val debugMCPattern = "MC([-\\+])([0-9]+)".r
 
     s match {
       case "PING" => ping()
@@ -32,6 +41,9 @@ object Messages {
       case directionPattern(sign, value) => direction(if(sign == "-") -value.toInt else value.toInt)
       case frontLightsPattern(value) => frontLights(value == "1")
       case sideLightsPattern(value) => sideLights(value == "1")
+      case debugMAPattern(sign, value) => debugMA(if(sign == "-") -value.toInt else value.toInt)
+      case debugMBPattern(sign, value) => debugMB(if(sign == "-") -value.toInt else value.toInt)
+      case debugMCPattern(sign, value) => debugMC(if(sign == "-") -value.toInt else value.toInt)
       case _ => "Not implemented"
     }
   }
